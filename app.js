0381edda26303116e2a087e880ecc1c1c7efa2d1
Hesham <heshamomar820@gmail.com>
2021-01-23 02:08:48 +0200

    adding game over Scenario

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = Vue.createApp({
         playerHealth: 100,
         monsterHealth: 100,
         currentRound: 0,
+        winner: null,
+
         }
     },
     computed: {
@@ -19,6 +21,26 @@ const app = Vue.createApp({
         mayUseSpecialAttack(){
             return this.currentRound % 3 !== 0
         }
+    },
+    watch: {
+        playerHealth(value){
+            if (value <= 0 && this.monsterHealth <= 0) {
+                //draw
+                this.winner = 'draw';
+            } else if (value <= 0 ) {
+                //monster win
+                this.winner = 'monster';
+            } 
+        },
+        monsterHealth(value){
+            if (value <= 0 && this.playerHealth <= 0) {
+                //draw
+                this.winner = 'draw';
+            } else if (value <= 0 ) {
+                // player win
+                this.winner = 'player';
+            } 
+        },
     },
     methods: {
         attackMonster(){
@@ -46,7 +68,8 @@ const app = Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
-        }
+        },
+
     }
 });
 
