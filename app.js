8f8b9c5dc0462336dd8d347a7e30a6a040ccb08f
Hesham <heshamomar820@gmail.com>
2021-01-25 07:46:08 -0800

    final touch

--- app.js
+++ app.js
@@ -2,6 +2,42 @@ function gatRandomValue(min, max){
     return Math.floor(Math.random() * (max - min) ) + min;
 };
 const app = Vue.createApp({
+    template: `
+        <section id="monster" class="container">
+        <h2>Monster Health</h2>
+        <div class="healthbar">
+        <div class="healthbar__value" :style="monsterBarStyles"></div>
+        </div>
+    </section>
+    <section id="player" class="container">
+        <h2>Your Health</h2>
+        <div class="healthbar">
+        <div class="healthbar__value" :style="playerBarStyles"></div>
+        </div>
+    </section>
+    <section class="container" v-if="winner">
+        <h2>Game Over!</h2>
+        <h3 v-if="winner ==='monster'">Monnster Won! <br><img class="wining-svg" src="img/monster.svg" alt="monster"></h3>
+        <h3 v-else-if="winner ==='player'"> You Won! <br><img class="wining-svg" src="img/player.svg" alt="player"> </h3>
+        <h3 v-else="winner ==='draw'">It's a draw! <br><img class="wining-svg" src="img/white-flag.svg" alt="draw"></h3>
+        <button class="newgame-button" @click="startGame">Start New Game</button>
+    </section>
+    <section id="controls" v-else>
+        <button @click="attackMonster">ATTACK <img class="button-svg" src="img/attack.svg" alt=""></button>
+        <button :disabled="mayUseSpecialAttack" @click="specialAttackMonster">SPECIAL ATTACK <img class="button-svg" src="img/special.svg" alt=""></button>
+        <button @click="healPlayer">HEAL <img class="button-svg" src="img/heal.svg" alt=""></button>
+        <button @click="surrender">SURRENDER <img class="button-svg" src="img/surrender.svg" alt=""></button>
+    </section>
+    <section id="log" class="container">
+        <h2>Battle Log</h2>
+        <ul >
+        <li v-for="logMessage in logMessages">
+            <span :class="{'log--player': logMessage.actionBy === 'player', 'log--monster': logMessage.actionBy === 'monster'}">{{ logMessage.actionBy === 'player' ? 'Player' : 'Monter' }}</span>
+            <span v-if="logMessage.actionType === 'heal'"> heals himself for <span class="log--heal">{{ logMessage.actionValue }}</span></span>
+            <span v-else> attacks and deals <span class="log--damage">{{ logMessage.actionValue}}</span></span>
+        </li>
+        </ul>
+    </section>`,   
     data() {
         return{
         playerHealth: 100,
